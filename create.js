0762d28809428ff66eb330d885d7f47ebf71a0de
Robert Moores <rob@ambiently.co.uk>
2021-01-25 03:07:35 +0000

    Adding auth to our Serverless API

--- create.js
+++ create.js
@@ -1,5 +1,5 @@
-import * as uuid from "uuid";
 import handler from "./libs/handler-lib";
+import * as uuid from "uuid";
 import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
@@ -8,7 +8,7 @@ export const main = handler(async (event, context) => {
     TableName: process.env.tableName,
     Item: {
       // The attributes of the item to be created
-      userId: "123", // The id of the author
+      userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
       noteId: uuid.v1(), // A unique uuid
       content: data.content, // Parsed from request body
       attachment: data.attachment, // Parsed from request body
